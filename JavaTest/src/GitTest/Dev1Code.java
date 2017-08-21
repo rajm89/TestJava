8a3f3be7c4121f3318ad5ab3577ee4259adc5f91
raj <raj@raj.com>
2017-08-21 19:32:41 +0530

    Initial commit

---
+++ JavaTest/src/GitTest/Dev1Code.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package GitTest;
+
+/**
+ * @author Admin
+ *
+ */
+public class Dev1Code {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("This is first line test");
+			System.out.println("This is another test line");
+			
+			
+			
+	}
+
+}
