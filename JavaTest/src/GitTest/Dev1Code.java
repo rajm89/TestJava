39a1c09ecc11e8d314a60eda1e80f62c485c98c3
raj <raj@raj.com>
2017-08-21 20:50:05 +0530

    fxix cap

--- JavaTest/src/GitTest/Dev1Code.java
+++ JavaTest/src/GitTest/Dev1Code.java
@@ -15,7 +15,7 @@ public class Dev1Code {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("This is first line test");
-			System.out.println("This is another test line");
+			System.out.println("This is ANOTHER TESTline");
 			
 			System.out.println("This is third test line");
 			System.out.println("This is forth test line");
